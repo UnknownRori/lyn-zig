b736b1bb4c50876a017ee330b35add27649ff801
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-06-09 06:17:00 +0700

    refactor and add additional http method

--- src/app/router.zig
+++ src/app/router.zig
@@ -11,25 +11,10 @@ const Handler = struct {
     path: []const u8,
     instance: usize,
     handler: usize,
-};
 
-pub const Router = struct {
-    _routes: std.ArrayList(Handler),
-    _allocator: mem.Allocator,
     const Self = @This();
 
-    pub fn init(allocator: mem.Allocator) Self {
-        return Self{
-            ._routes = std.ArrayList(Handler).init(allocator),
-            ._allocator = allocator,
-        };
-    }
-
-    pub fn deinit(self: Self) void {
-        self._routes.deinit();
-    }
-
-    pub fn get(self: *Self, path: []const u8, instance: *anyopaque, handler: anytype) !void {
+    pub fn init(method: root.HTTPMethod, path: []const u8, instance: *anyopaque, handler: anytype) !Handler {
         // INFO : Inspired from https://github.com/zigzap/zap/blob/master/src/router.zig
         comptime {
             const handlerInfo = @typeInfo(@TypeOf(handler));
@@ -68,12 +53,49 @@ pub const Router = struct {
             }
         }
 
-        try self._routes.append(Handler{
+        return Self{
             .path = path,
             .instance = @intFromPtr(instance),
             .handler = @intFromPtr(&handler),
-            .method = root.HTTPMethod.GET,
-        });
+            .method = method,
+        };
+    }
+};
+
+pub const Router = struct {
+    _routes: std.ArrayList(Handler),
+    _allocator: mem.Allocator,
+    const Self = @This();
+
+    pub fn init(allocator: mem.Allocator) Self {
+        return Self{
+            ._routes = std.ArrayList(Handler).init(allocator),
+            ._allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: Self) void {
+        self._routes.deinit();
+    }
+
+    pub fn get(self: *Self, path: []const u8, instance: *anyopaque, handler: anytype) !void {
+        const handlerObj = try Handler.init(root.HTTPMethod.GET, path, instance, handler);
+        try self._routes.append(handlerObj);
+    }
+
+    pub fn post(self: *Self, path: []const u8, instance: *anyopaque, handler: anytype) !void {
+        const handlerObj = try Handler.init(root.HTTPMethod.POST, path, instance, handler);
+        try self._routes.append(handlerObj);
+    }
+
+    pub fn patch(self: *Self, path: []const u8, instance: *anyopaque, handler: anytype) !void {
+        const handlerObj = try Handler.init(root.HTTPMethod.PATCH, path, instance, handler);
+        try self._routes.append(handlerObj);
+    }
+
+    pub fn delete(self: *Self, path: []const u8, instance: *anyopaque, handler: anytype) !void {
+        const handlerObj = try Handler.init(root.HTTPMethod.DELETE, path, instance, handler);
+        try self._routes.append(handlerObj);
     }
 
     pub fn resolve(self: *Self, req: root.Request, res: *root.Response) !void {
